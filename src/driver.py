875c8e578d6473b0c181a1c4ec61d67355d261c9
Pesanteur <pesanteur123@gmail.com>
2021-01-04 23:49:51 -0500

    Reviewing driver.py

--- src/driver.py
+++ src/driver.py
@@ -1,20 +1,46 @@
 import getpass
 import logging
 import os
+import pathlib
+import random
+import re
 import requests
+import shutil
+import string
+import subprocess
 
+from abc import ABC, abstractmethod
 from selenium import webdriver
 
+user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
+
 class HttpGetResponse:
-    def __init__(self, text, url):
+    def __init__(self, text, url, **kwargs):
         self.text = text
         self.url = url
+        self.status_code = kwargs.get('status_code', None)
 
-class SeleniumDriver:
-    def __init__(self, timeout):
-        self.timeout = timeout
+class Driver(ABC):
+    def __init__(self, **kwargs):
+        self.data_dir = kwargs.get('data_dir')
+        self.timeout = kwargs.get('timeout')
 
-        self.driver_path = '/usr/bin/chromedriver'
+        @abstractmethod
+        def get(self, url) -> HttpGetResponse:
+            pass
+
+class SeleniumDriver(Driver):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.selenium_path = pathlib.Path('selenium').resolve()
+        self.selenium_path.mkdir(exist_ok=True)
+        self.driver_path = self.selenium_path / 'chromedriver'
+        driver_paths = [
+            '/usr/bin/chromedriver',
+            '/usr/local/bin/chromedriver'
+        ]
+        ### CONTINUE FROM HERE
+
         if not os.path.exists(self.driver_path):
             raise Exception(f'not found: {self.driver_path}')
 
